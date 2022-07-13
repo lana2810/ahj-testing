44920cb10b945975d9ba45cc6a15e5f3f475d76b
Svetlana Sviridova <sviridova_sv@inbox.ru>
2022-07-13 12:32:12 +0300

    add e2e.server

--- src/js/e2e/e2e.test.js
+++ src/js/e2e/e2e.test.js
@@ -5,9 +5,19 @@ jest.setTimeout(60000);
 describe("test validator form", () => {
   let browser = null;
   let page = null;
+  let server = null;
   const baseUrl = "http://localhost:8888";
 
   beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on("error", reject);
+      server.on("message", (message) => {
+        if (message === "ok") {
+          resolve();
+        }
+      });
+    });
     browser = await puppetteer.launch({
       // headless: false, // show gui
       // slowMo: 100,
@@ -17,6 +27,7 @@ describe("test validator form", () => {
   });
   afterAll(async () => {
     await browser.close();
+    server.kill();
   });
   test("test visa", async () => {
     await page.goto(baseUrl);
